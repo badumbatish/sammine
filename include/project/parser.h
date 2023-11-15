9d9ae55d6ae9ffd2ecd94a7c4fdeecbea682ece2
Jasmine <tanghocle456@gmail.com>
2023-11-14 20:30:29 -0800

    First start on implementing my own

--- include/project/parser.h
+++ include/project/parser.h
@@ -7,52 +7,8 @@
 #include "AST.h"
 #include "lexer.h"
 #include <map>
+
 class parser {
 public:
-    static inline int CurTok {};
 
-    static int getNextToken();
-
-    static inline std::map<char, int> BinopPrecedence = {
-            {'<', 10},
-            {'+', 20},
-            {'-', 20},
-            {'*', 40}
-    };
-
-
-    static std::unique_ptr<ExprAST> LogError(const char *Str);
-
-    static std::unique_ptr<PrototypeAST> LogErrorP(const char *Str);
-
-    static std::unique_ptr<ExprAST> ParseExpression();
-
-    static std::unique_ptr<ExprAST> ParseNumberExpr();
-
-    static std::unique_ptr<ExprAST> ParseParenExpr();
-
-    static std::unique_ptr<ExprAST> ParseIdentifierExpr();
-
-    static std::unique_ptr<ExprAST> ParsePrimary();
-
-    static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<ExprAST> LHS);
-
-    static std::unique_ptr<FunctionAST> ParseDefinition();
-
-    static std::unique_ptr<PrototypeAST> ParseExtern();
-
-    static std::unique_ptr<FunctionAST> ParseTopLevelExpr();
-    static std::unique_ptr<PrototypeAST> ParsePrototype();
-
-    static void MainLoop();
-    static void InitializeModule();
-    static void HandleDefinition();
-
-    static int GetTokPrecedence();
-
-    static void HandleExtern();
-
-    static void HandleTopLevelExpression();
-
-    static llvm::Value* LogErrorV(const char *Str);
 };
--- src/project/main.cpp
+++ src/project/main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
 #include "lexer.h"
 #include "parser.h"
+
 int main() {
-
-    // Prime the first token.
-    fprintf(stderr, "ready> ");
-    parser::getNextToken();
-
-    // Make the module, which holds all the code.
-    parser::InitializeModule();
-
-    // Run the main "interpreter loop" now.
-
-    parser::MainLoop();
-
-    // Print out all of the generated code.
-    TheModule->print(llvm::errs(), nullptr);
 
     return 0;
 }
